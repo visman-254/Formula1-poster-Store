767570f8acfe09921e3a6cf7eb89f5e5739d8d71
visman-254 <kiptooelvis084@gmail.com>
2026-02-02 13:03:42 +0300

    Done something that will m,ake sure logs work

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,7 +1,7 @@
 let API_BASE;
 
 if (import.meta.env.MODE === "production") {
-  API_BASE = "https://api.pannamusic.co.ke";
+  API_BASE = "https://formula1-poster-store-1.onrender.com";
 } else {
   API_BASE = "http://localhost:5000";
 }
